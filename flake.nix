2dc247ec71a87e988fac8a20256459b508e78ddf
Colt Frederickson <coltfred@gmail.com>
2023-09-07 11:32:56 -0600

    Add nix flake and AsRef<[u8]> (#3)
    * Add nix flake and AsRef
    
    * Update flake.nix
    
    Co-authored-by: Craig Colegrove <34786857+giarc3@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Craig Colegrove <34786857+giarc3@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Base64 type";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rusttoolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      rec {
+        # nix develop
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [ rusttoolchain pkg-config pkgs.openssl ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+              [ pkgs.darwin.apple_sdk.frameworks.Security ];
+        };
+
+      });
+}
